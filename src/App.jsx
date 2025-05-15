5213e8b856ffd630956e56335495de42e60566a7
Mohamed Abubakr <113220329+ganiny@users.noreply.github.com>
2025-05-15 11:19:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,16 @@ import Country from "./pages/Country";
 import NoPage from "./pages/NoPage";
 
 function App() {
-  
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path=":countryName" element={<Country />}/>
-          <Route path="*" element={<NoPage />}/>
+          <Route path=":countryName" element={<Country />} />
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
-   
-      
-        
-      
-    
   );
 }
 
